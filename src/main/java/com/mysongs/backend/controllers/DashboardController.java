4a71f309df9166e80e74f3716023177ac5b5cd31
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-05-03 18:09:26 +0200

    main page now called dashboard

--- src/main/java/com/mysongs/backend/controllers/DashboardController.java
+++ src/main/java/com/mysongs/backend/controllers/DashboardController.java
@@ -1,16 +1,16 @@
 package com.mysongs.backend.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
-
 @Controller
 @RequestMapping("/")
-public class HomeController {
+public class DashboardController {
+
     @GetMapping
-    public String dashboard() {
+    public String dashboard(Model model) {
         return "dashboard";
     }
     
