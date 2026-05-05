99230799f37558a7347ab29fbadbbdb84322b3c8
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-05-05 16:57:26 +0200

    bootstrap setup and import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './scss/style.scss'
+import * as bootstrap from 'bootstrap'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
